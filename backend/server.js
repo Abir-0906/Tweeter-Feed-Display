992d826508fa8063619eb28abf05458977e2b684
Abir Banik <abirbanik65@gmail.com>
2025-07-16 05:04:55 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-const app = express();
 const cors = require('cors');
 const tweetRoutes = require('./routes/twitterRoutes');
-// Init
+
+const app = express();
+
+// Connect MongoDB
 connectDB();
+
+// CORS setup
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://neon-rugelach-685a13.netlify.app'
+  ],
   credentials: true,
 }));
+
+// Middleware
 app.use(express.json());
 
-
-
-
-// Test route
+// Test Route
 app.get('/', (req, res) => res.send('API Running'));
 
-
-
-
-
-
-// Routes
+// API Routes
 app.use('/api/twitter', tweetRoutes);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
